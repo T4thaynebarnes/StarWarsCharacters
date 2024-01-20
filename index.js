93030e283670d932b6fb038a6154bb7ded504288
T4thaynebarnes <thaynebarnes@gmail.com>
2024-01-20 09:20:15 -0700

    Everything is functioning except the PUT method, I tried ajax call and fecth calls, still the lab solution for put methods did not function either. researched adn did my best I need to move on and come back to this later

--- index.js
+++ index.js
@@ -18,14 +18,15 @@ let html = (strings, ...values) =>
   strings.reduce((acc, str, i) => acc + str + (values[i] || ""), "");
 //lit-html snippet - End
 
-// **** TESTING CODE***
+// **** URL endpoint***
 const URL_ENDPOINT = "http://localhost:3000/StarWarsCharacters";
-// testing JQUERY get method
+// testing JQUERY get method so one can see what data is being passed in
 $.get(URL_ENDPOINT).then((data) => console.log("testing url endpoint", data));
 
 // get method to grab the data in the json file and display it in a table
 $.get(URL_ENDPOINT).then((data) =>
-  // data.map() function will do the same as for/of loop
+  // data.map() function will do the same as for/of loop. it basically goes
+  // thru each and every item in the array in data.json and displays it on the HTML
   data.map((character) => {
     console.log(
       "requesting data and mapping it onto the UI,same as for/of loop"
@@ -47,7 +48,7 @@ $.get(URL_ENDPOINT).then((data) =>
           </button>
         </td>
         <td>
-          <button class="btn btn-info">&#9999 </button>
+          <button onclick="updateCharacter('${character.id}')"  id="update-btn-${character.id}" class="btn btn-info">&#9999 </button>
         </td>
       </tr>`)
     );
@@ -62,13 +63,34 @@ function deleteCharacter(id) {
   });
 }
 // ****end of deleteCharacter()**
-// this function handles the update of an existing character in the API
+// *** function to update character use put method, it is called when the edit button is clicked
+function updateCharacter() {
+  $("#update-form").append(
+    $(html`
+      <div class="form-control">
+        <h1>update your character</h1>
+        <label for="update-name">Name: </label>
+        <input id="update-name" type="text" placeholder="enter new name here" />
+        <label for="update-profession">Profession: </label>
+        <input
+          id="update-profession"
+          type="text"
+          placeholder="enter new profession here"
+        />
+        <button class="btn btn-danger mt-2" id="submit-changes-btn">
+          Submit Changes &#9854;
+        </button>
+      </div>
+    `)
+  );
+}
 
 // this button once clicked will post a new character to the API
 // event listener for submit button on screen
+
 $("#create-newcharacter-btn").click(function () {
   console.log(
-    "creating new character adding value from input bozes to json server"
+    "creating new character adding value from input boxes to json server"
   );
   fetch(URL_ENDPOINT, {
     method: "POST",
@@ -81,3 +103,36 @@ $("#create-newcharacter-btn").click(function () {
     },
   });
 });
+// **** end of submit your character button
+// ***I have tried so hard to get a put method to work wether it be Jquery or fetch method.
+// I am not sure why it is not working I also was never able to get the lab solution for week 12 to work to use as a reference.
+// i tried fecth method as you see below, I tried ajax call, I beleive it is a logical error I have nor error messages or indications of syntax typos
+$("#submit-changes-btn").click(function () {
+  fetch(URL_ENDPOINT, {
+    method: "PUT",
+    body: JSON.stringify({
+      name: $("#update-name").val(),
+      profession: $("#update-profession").val(),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+});
+
+// ***** this ajax call did not work for put method
+// $("submit-changes-btn").click(function () {
+//   return $.ajax({
+//     url: URL_ENDPOINT + `/${id}`,
+//     type: "PUT",
+//     name: $("#update-name").val(),
+//     profession: $("#update-profession").val(),
+
+//     success: function (response) {
+//       console.log(response);
+//     },
+//     error: function (xhr, status, error) {
+//       console.log(xhr.responseText);
+//     },
+//   });
+// });
